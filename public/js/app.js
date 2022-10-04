97f21b5b5dcdf4c1e4c29e9d387fd520cfb9167f
Gabridg <gabri99.del@gmail.com>
2022-10-04 15:22:47 +0200

    composer / npm install. connected t db

--- public/js/app.js
+++ public/js/app.js
@@ -37407,8 +37407,8 @@ imageField.addEventListener('input', function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laravel\laravel-auth\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laravel\laravel-auth\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laravel\laravel-auth-master\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laravel\laravel-auth-master\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
