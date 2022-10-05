609a1f8eb676ee6f870ea697e64c5046384a25ab
Gabridg <gabri99.del@gmail.com>
2022-10-05 17:49:42 +0200

    task completed + css effects

--- resources/js/router.js
+++ resources/js/router.js
@@ -4,6 +4,7 @@ import VueRouter from 'vue-router';
 import HomePage from './components/pages/HomePage.vue';
 import AboutPage from './components/pages/AboutPage.vue';
 import ContactsPage from './components/pages/ContactsPage.vue';
+import PostDetailPage from './components/pages/PostDetailPage.vue';
 import ErrorPage from './components/pages/ErrorPage.vue';
 
 Vue.use(VueRouter)
@@ -19,6 +20,9 @@ const routes = new VueRouter({
         },
         {
             path: '/contacts', component: ContactsPage, name: 'contacts'
+        },
+        {
+            path: '/posts/:id', component: PostDetailPage, name: 'post-detail'
         },
         {
             path: '/*', component: ErrorPage
